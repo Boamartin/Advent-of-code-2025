706df558b8d1adca4ced6141e06aad9d2ea46912
Martin <martin.boa102@gmail.com>
2025-12-04 20:02:44 +0100

    day4 part1

--- day4/part1/src/main.c
+++ day4/part1/src/main.c
@@ -12,16 +12,22 @@ typedef struct {
     int cols;
 } map_t;
 
+int dRow[8] = {-1, -1, -1, 0, 1, 1,  1,  0};
+int dCol[8] = {-1,  0,  1, 1, 1, 0, -1, -1};
+
 map_t map = {0};
 
 void printMap(map_t map);
 char getChar(map_t map, int row, int col);
+void addPadding(map_t *map);
+int getNeighbours(map_t* map, int row, int col);
 
 void main(void)
 {
-    FILE *file = fopen("input/test1.txt", "r");
-    // FILE *file = fopen("input/input1.txt", "r");
+    // FILE *file = fopen("input/test1.txt", "r");
+    FILE *file = fopen("input/input1.txt", "r");
     
+    int ans = 0;
     char line[MAX_MAP_SIZE];
 
     while (map.rows < MAX_MAP_SIZE && fgets(line, sizeof(line), file)) {
@@ -32,12 +38,42 @@ void main(void)
         map.cols = strlen(line);
         map.rows++;
     }
-
     fclose(file);
 
+    addPadding(&map);
     printMap(map);
+    int nNeighbours;
 
-    // Loop through map and check all eight neighbours
+    // nNeighbours = getNeighbours(&map, 1, 1);
+    // printf("%d ", nNeighbours);
+
+    for (int row = 1; row < map.rows-1; row++)
+    {
+        for (int col = 1; col < map.cols-1; col++)
+        {
+            
+            // check if there is a roll of paper at this location
+            if(getChar(map, row, col) == '@')
+            {
+                nNeighbours = getNeighbours(&map, row, col);
+                if(nNeighbours < 4)
+                {
+                    ans ++;
+                    printf("x");
+                }
+                else
+                {
+                    printf("@");
+                }
+            }
+            else
+            {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+    printf("%d rolls can be accessed by a forklifts.\n", ans);
 }
 
 void printMap(map_t map)
@@ -52,3 +88,49 @@ char getChar(map_t map, int row, int col)
     return map.data[row][col];
 }
 
+void addPadding(map_t *map)
+{
+    int r, c;
+    char pad = '.';
+
+    // Shift down and right by 1
+    for (r = map->rows - 1; r >= 0; r--) {
+        for (c = map->cols - 1; c >= 0; c--) {
+            map->data[r + 1][c + 1] = map->data[r][c];
+        }
+    }
+
+    // Top border
+    for (c = 0; c < map->cols + 2; c++)
+        map->data[0][c] = pad;
+
+    // Bottom border
+    for (c = 0; c < map->cols + 2; c++)
+        map->data[map->rows + 1][c] = pad;
+
+    // Left + right borders
+    for (r = 1; r < map->rows + 1; r++) {
+        map->data[r][0] = pad;
+        map->data[r][map->cols + 1] = pad;
+        map->data[r][map->cols + 2] = '\0';
+    }
+
+    map->rows += 2;
+    map->cols += 2;
+}
+
+int getNeighbours(map_t* map, int row, int col)
+{
+    int ans = 0;
+    char currentChar;
+    for (int i = 0; i < 8; i++)
+    {
+        currentChar = getChar(*map, row + dRow[i], col + dCol[i]);
+        // printf("current char = %c\n", currentChar);
+        if(currentChar == '@')
+        { 
+            ans++; 
+        }
+    }
+    return ans;
+}
